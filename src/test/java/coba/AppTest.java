3b53279c0fb8e9ae4f56ece8e74c572547f2e4c9
vikhanmuhammad <muhammadvikhanmuharram@gmail.com>
2024-06-07 02:07:50 +0700

    first commit

---
+++ src/test/java/coba/AppTest.java
@@ -0,0 +1,32 @@
+package src.test.java.coba;
+
+import static org.junit.Assert.assertEquals;
+import src.main.java.coba.TanggalHelper;
+import org.junit.Test;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testTambahMenit() {
+        LocalDateTime waktu = LocalDateTime.of(2023, 6, 1, 12, 0, 0);
+        String expected = "2023-06-01 12:10:00";
+        String actual = TanggalHelper.tambahMenit(waktu, 10);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTambahJam() {
+        LocalDateTime waktu = LocalDateTime.of(2023, 6, 1, 12, 0, 0);
+        String expected = "2023-06-01 15:00:00";
+        String actual = TanggalHelper.tambahJam(waktu, 3);
+        assertEquals(expected, actual);
+    }
+}
